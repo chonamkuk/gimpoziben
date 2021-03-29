85074662546b479dd43ac203d02fca07c44537d7
jsh <secondlinedragonfly@gmail.com>
2021-03-29 22:59:13 +0900

    e커머스 디자인 적용1

---
+++ src/main/java/kr/co/kimpoziben/controller/ShopController.java
@@ -0,0 +1,20 @@
+package kr.co.kimpoziben.controller;
+
+import kr.co.kimpoziben.dto.SearchDto;
+import kr.co.kimpoziben.util.PageRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/shop")
+public class ShopController {
+
+    @GetMapping("/list.do")
+    public String list(Model model, final PageRequest pageable, SearchDto searchDto) throws Exception {
+        return "/shop/list";
+    }
+}
