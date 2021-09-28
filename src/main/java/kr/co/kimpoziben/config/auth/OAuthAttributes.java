1687fdea01cbf64d52e0b62da3a61b717e43a0c4
unasd <secondlinedragonfly@gmail.com>
2021-09-28 23:03:11 +0900

    로그인기능 정리

--- src/main/java/kr/co/kimpoziben/config/auth/OAuthAttributes.java
+++ src/main/java/kr/co/kimpoziben/config/auth/OAuthAttributes.java
@@ -56,7 +56,7 @@ public class OAuthAttributes {
                 .name(name)
                 .email(email)
                 .mobile(mobile)
-                .userRole(UserRole.GUEST)
+                .userRole(UserRole.USER)
                 .build();
 
     }
